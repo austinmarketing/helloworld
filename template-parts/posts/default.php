929afb24cf843858ed80bc395dac7185e07d38d8
austinmarketing <37705361+austinmarketing@users.noreply.github.com>
2018-04-09 10:23:34 +0100

    Add files via upload

---
+++ template-parts/posts/default.php
@@ -0,0 +1,77 @@
+			<div id="content">
+
+				<div id="inner-content" class="grid-container">
+
+					<main id="main" class="grid-70" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+						<?php
+						   /*
+							* This is the default post format.
+							*/
+						?>
+
+						<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
+
+							<header class="article-header entry-header">
+
+								<h1 class="entry-title single-title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
+
+								<p class="byline entry-meta vcard">
+
+									<?php printf( __( 'Posted', 'helloworld' ).' %1$s %2$s',
+				                       /* the time the post was published */
+				                       '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+				                       /* The author of the post is disabled by default remove the comment slashes below to use */
+				                       //'<span class="by">'.__( 'by', 'helloworld' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+				                    ); ?>
+
+								</p>
+
+							</header> <?php // end article header ?>
+
+							<section class="entry-content cf" itemprop="articleBody">
+							<?php
+								// the content (pretty self explanatory huh)
+								the_content();
+							?>
+							</section> <?php // end article section ?>
+
+							<footer class="article-footer">
+
+								<?php printf( __( 'filed under', 'helloworld' ).': %1$s', get_the_category_list(', ') ); ?>
+
+								<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'helloworld' ) . '</span> ', ', ', '</p>' ); ?>
+
+								</footer> <?php // end article footer ?>
+
+								<?php //comments_template(); ?>
+
+								</article> <?php // end article ?>
+
+						<?php endwhile; ?>
+
+						<?php else : ?>
+
+							<article id="post-not-found" class="hentry cf">
+									<header class="article-header">
+										<h1><?php _e( 'Page Not Found', 'helloworld' ); ?></h1>
+									</header>
+									<section class="entry-content">
+										<p><?php _e( 'Something is missing. Try double checking your settings.', 'helloworld' ); ?></p>
+									</section>
+									<footer class="article-footer">
+											<p><?php _e( 'This is the error message in the single.php template.', 'helloworld' ); ?></p>
+									</footer>
+							</article>
+
+						<?php endif; ?>
+
+					</main>
+
+					<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
